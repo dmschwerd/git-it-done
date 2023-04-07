24cbcb4f92d6195b0b3888ee504517af56533f7f
David <dmsch656@gmail.com>
2023-04-07 10:14:25 -0700

    username field takes input and sends to get method

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,6 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
 var getUserRepos = function(user) {
     // github api url format
     var apiUrl = "http://api.github.com/users/" + user + "/repos";
@@ -8,5 +11,19 @@ var getUserRepos = function(user) {
         });
     });
 };
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    var username = nameInputEl.value.trim();
+
+    if(username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub Username");
+    }
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
 
 getUserRepos("dmschwerd");
